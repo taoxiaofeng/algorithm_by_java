58dac72add6553f6a47c88f79043d47642f59eb7
taoxiaofeng <taoxiaofeng55@qq.com>
2026-01-18 09:57:23 +0800

    重构 SwitchDemo 示例程序，移除星期几判断逻辑，新增月份天数判断功能

--- demo/src/SwitchDemo.java
+++ demo/src/SwitchDemo.java
@@ -4,38 +4,66 @@ public class SwitchDemo {
 
     Scanner scanner = new Scanner(System.in);
     
-    System.out.println("请输入一个整数 (1-7) 代表星期几:");
+    // System.out.println("请输入一个整数 (1-7) 代表星期几:");
 
-    int day = scanner.nextInt();
-    String dayName;
+    // int day = scanner.nextInt();
+    // String dayName;
 
-    switch (day) {
+    // switch (day) {
+    //   case 1:
+    //     dayName = "Monday";
+    //     break;
+    //   case 2:
+    //     dayName = "Tuesday";
+    //     break;
+    //   case 3:
+    //     dayName = "Wednesday";
+    //     break;
+    //   case 4:
+    //     dayName = "Thursday";
+    //     break;
+    //   case 5:
+    //     dayName = "Friday";
+    //     break;
+    //   case 6:
+    //     dayName = "Saturday";
+    //     break;
+    //   case 7:
+    //     dayName = "Sunday";
+    //     break;
+    //   default:
+    //     dayName = "Invalid day";
+    //     break;
+    // }
+
+    // System.out.println("The day is: " + dayName);
+
+    System.out.println("请输入一个月份 (1-12):");
+    int month = scanner.nextInt();
+
+    // case 穿透
+    switch (month) {
       case 1:
-        dayName = "Monday";
-        break;
-      case 2:
-        dayName = "Tuesday";
-        break;
       case 3:
-        dayName = "Wednesday";
+      case 5:
+      case 7:
+      case 8:
+      case 10:
+      case 12:
+        System.out.println(month + " 月有 31 天");
         break;
       case 4:
-        dayName = "Thursday";
-        break;
-      case 5:
-        dayName = "Friday";
-        break;
       case 6:
-        dayName = "Saturday";
+      case 9:
+      case 11:
+        System.out.println(month + " 月有 30 天");
         break;
-      case 7:
-        dayName = "Sunday";
+      case 2:
+        System.out.println(month + " 月有 28 或 29 天");
         break;
       default:
-        dayName = "Invalid day";
+        System.out.println("输入月份无效");
         break;
     }
-
-    System.out.println("The day is: " + dayName);
   }
 }
